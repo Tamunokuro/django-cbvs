a74ba8f84473bcec90cfa437a3008be502a4b166
Tamunokuro <tamunokrobluejack@gmail.com>
2022-08-15 16:59:18 +0100

    NC

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('', views.ArtistView.as_view(), name='home'),
     path('create/', views.ArtistCreateView.as_view(), name='create_artist'),
     path('artists/',views.ArtistListView.as_view(), name='artist_list'),
-    path('artist/<int:pk>/',views.ArtistDetailView.as_view(), name='artist_detail')
+    path('artist/<int:pk>/',views.ArtistDetailView.as_view(), name='artist_detail'),
+    path('add-edit/<int:pk>/album/edit/', views.ArtistEditView.as_view(), name='album-edit')
 
 
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,9 @@ import imp
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, FormView
 from django.contrib import messages
+from django.views.generic.detail import SingleObjectMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Artist
 from .forms import ArtistCollection
@@ -32,5 +35,32 @@ class ArtistCreateView(CreateView):
             'Artist Created Successfully'
         )
         return super().form_valid(form)
+
+class ArtistEditView(SingleObjectMixin, FormView):
+    model = Artist
+    template_name = 'artist_collection.html'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset= Artist.objects.all())
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset= Artist.objects.all())
+        return super().post(request, *args, **kwargs)
+
+    def get_form(self, form_class=None):
+        return ArtistCollection(**self.get_form_kwargs(), instance=self.object)
+
+    def get_success_url(self):
+        return reverse('core:artist_detail', kwargs={'pk':self.object.pk})
     
+    def form_valid(self, form):
+        form.save()
+
+        messages.add_message(
+            self.request,
+            messages.SUCCESS,
+            'Successfull'
+        )
+        return HttpResponseRedirect(self.get_success_url())
 
